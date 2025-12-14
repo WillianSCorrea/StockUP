ae21f0f2f643a0ab037f1b9305eab8c5c885eed5
Willian da Silva Correa <163740121+WillianSCorrea@users.noreply.github.com>
2025-12-14 15:20:24 +0000

    Configuração do Next e Tailwind

---
+++ stock_up/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './app/**/*.{ts,tsx,js,jsx}',
+    './components/**/*.{ts,tsx,js,jsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3B82F6'
+      }
+    }
+  },
+  plugins: []
+}
