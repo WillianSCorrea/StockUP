51ac8b77307419a350c937034bcdf2d405eaa19e
Willian da Silva Correa <163740121+WillianSCorrea@users.noreply.github.com>
2025-12-17 14:28:50 +0000

    chore: update dependencies in package.json
    - bump next from 16.0.8 to ^16.0.10
    - update @tailwindcss/postcss from ^4 to ^4.1.18
    - add autoprefixer as a new dependency with version ^10.4.23
    - add postcss as a new dependency with version ^8.5.6

--- stock_up/app/layout.tsx
+++ stock_up/app/layout.tsx
@@ -1,36 +1,10 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../styles/globals.css";
+import React from 'react'
+import './globals.css'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Stock Up",
-  description: "An inventory management app for small businesses, and, built with Next.js and Tailwind CSS.",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="pt-BR">
+      <body>{children}</body>
     </html>
-  );
-
-  
+  )
 }
